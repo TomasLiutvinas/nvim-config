4222ea62c36566f098a23bd0999e6bff5652ac51
Tomsen <tehdreamwalker@gmail.com>
2024-10-06 23:04:19 +0300

    i guess removing eslint

--- lua/tomsen/plugins/lsp.lua
+++ lua/tomsen/plugins/lsp.lua
@@ -30,7 +30,6 @@ return {
         "rust_analyzer",
         "gopls",
         "omnisharp",
-        "eslint",
         "html",
         "cssls",
       },
@@ -65,19 +64,6 @@ return {
                 }
               }
             }
-          }
-        end,
-
-        ["eslint"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.eslint.setup {
-            capabilities = capabilities,
-            settings = {
-              validate = "on",
-              packageManager = "yarn",
-            },
-            root_dir = lspconfig.util.root_pattern(".eslintrc.js", ".eslintrc.json", "package.json"),
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
           }
         end,
       }
